98ff8e607ec104b76f0c1e99c24d654e632a63ac
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2025-02-19 23:08:31 -0500

    remove sudo

--- dagster_cloud_pre_install.sh
+++ dagster_cloud_pre_install.sh
@@ -4,10 +4,10 @@ set -e
 echo "Starting dagster_cloud_pre_install.sh..."
 
 # Update package lists
-sudo apt-get update
+apt-get update
 
 # Install the unixODBC development package required by pyodbc
-sudo apt-get install -y unixodbc-dev
+apt-get install -y unixodbc-dev
 
 # Install the Python package pyodbc
 pip install pyodbc
